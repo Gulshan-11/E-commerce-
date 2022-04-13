5899197c50c4ce4df10e0961a4022e8a8390eb02
chaitanya <sparkscj234@gmail.com>
2022-04-13 11:09:25 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import './threed.js'
-import Spline from '@splinetool/react-spline'
+
 
 function App() {
   return (
--- src/threed.js
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function threed() {
-  return (
-    <div>
-	  <Spline scene="Loading..." />
-	</div>
-  );
-}
